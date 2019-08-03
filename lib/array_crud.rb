ea26280e6498253c81ce9f60ff5c15548a35580e
Kierra M <k.merriman32@hotmail.com>
2019-08-03 01:19:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   create_an_array = ["mommy", "daddy", "sister", "me"]
 end
 
-def add_element_to_end_of_array("wow", "I", "am", "really", "learning")
+def add_element_to_end_of_array(arry,element)
 add_element_to_end_of_array.push("arrays!")
 end
 
