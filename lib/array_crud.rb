0e729726c1b367cc0ed402481b78aa0fe52731b5
Kierra M <k.merriman32@hotmail.com>
2019-08-03 01:25:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(arry,element)
-add_element_to_end_of_array.push("arrays!")
+.push "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
