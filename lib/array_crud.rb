3aa07bd2c5c140bb1369b0988cfd086fc65ee428
Kierra M <k.merriman32@hotmail.com>
2019-08-03 01:34:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
  ["mommy", "daddy", "sister", "me"]
 end
 
-def add_element_to_end_of_array(arry,element)
-
+def add_element_to_end_of_array(array,element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
