ee9bb7782bef22375711b1f62fd66919b27bffff
Kierra M <k.merriman32@hotmail.com>
2019-08-03 01:19:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
 add_element_to_end_of_array.push("arrays!")
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(
   add_element_to_start_of_array.unshift ("cousin")
 end
 
